b6e328ed41de4ca54f029c22e5585dcdbe19d362
Chernov Sergey <chernov.sergey23@gmail.com>
2015-01-06 13:45:49 +0300

    first commit

---
+++ AlgorithmicHeights/List.h
@@ -0,0 +1,19 @@
+#ifndef LIST_H
+#define LIST_H
+
+template<typename T>
+class List{
+
+    List(){}
+    virtual ~List(){}
+
+    void push_front(T val){}
+    void push_back(T val){}
+
+    void pop_front(){}
+    void pop_back(){}
+
+    // to be continued...
+
+}
+#endif // LIST_H
---
+++ AlgorithmicHeights/Sortings.h
@@ -0,0 +1,17 @@
+#ifndef SORTINGS_H
+#define SORTINGS_H
+
+void MergeSort(){
+
+}
+
+void QuickSort(){
+
+}
+
+void HeapSort(){
+
+}
+
+
+#endif // SORTINGS_H
